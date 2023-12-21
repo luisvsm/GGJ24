bd159a1409e398e319da88f78194bc7a85a8bd2b
Luis Van Slageren <luisvsm@gmail.com>
2023-12-21 17:16:14 +1100

    Getting started on the build system

---
+++ Assets/Scripts/Editor/BuildScript.cs
@@ -0,0 +1,166 @@
+using UnityEditor;
+using System.Linq;
+using UnityEditor.Build.Reporting;
+using System;
+using UnityEngine;
+using System.Text.RegularExpressions;
+using System.IO;
+
+public class BuildScript
+{
+    static BuildOptions developmentBuild = BuildOptions.Development;
+
+    static string[] getScenes()
+    {
+        return EditorBuildSettings.scenes.Where(s => s.enabled).Select(s => s.path).ToArray();
+    }
+
+    [MenuItem("Build/Linux64 [Development]")]
+    static void PerformBuildLinux64()
+    {
+        string buildName = PlayerSettings.productName.Replace(" ", "-");
+        string buildFolder = "linux";
+        string extension = "x86_64";
+        string buildArtifact = $"./builds/{buildFolder}/{buildName}.{extension}";
+        BuildReport buildReport = null;
+        startLoggingErrors();
+
+        try{
+            buildReport = BuildPipeline.BuildPlayer(getScenes(), buildArtifact,
+                BuildTarget.StandaloneLinux64, developmentBuild);
+        }catch(Exception error){
+            Debug.LogError(error);
+        }
+
+        stopLoggingErrors();
+        
+        if(buildReport!= null && buildReport.summary.result == BuildResult.Succeeded){
+            System.IO.File.WriteAllText(@"./builds/lastbuild.txt", buildArtifact);
+        }else{
+            EditorApplication.Exit(1);
+        }
+    }
+
+
+    [MenuItem("Build/OSX [Development]")]
+    static void PerformBuildOSX()
+    {
+        string buildName = PlayerSettings.productName.Replace(" ", "-");
+        string buildFolder = "osx";
+        string extension = "app";
+        string buildArtifact = $"./builds/{buildFolder}/{buildName}.{extension}";
+        BuildReport buildReport = null;
+        startLoggingErrors();
+
+        try{
+            buildReport = BuildPipeline.BuildPlayer(getScenes(), buildArtifact,
+                BuildTarget.StandaloneOSX, developmentBuild);
+        }catch(Exception error){
+            Debug.LogError(error);
+        }
+
+        stopLoggingErrors();
+        
+        if(buildReport!= null && buildReport.summary.result == BuildResult.Succeeded){
+            System.IO.File.WriteAllText(@"./builds/lastbuild.txt", $"./builds/{buildFolder}");
+        }else{
+            EditorApplication.Exit(1);
+        }
+    }
+
+    [MenuItem("Build/Windows [Development]")]
+    static void PerformBuildWindows()
+    {
+        string buildName = PlayerSettings.productName.Replace(" ", "-");
+        string buildFolder = "windows";
+        string extension = "exe";
+        string buildArtifact = $"./builds/{buildFolder}/{buildName}.{extension}";
+        BuildReport buildReport = null;
+        startLoggingErrors();
+
+        try{
+            buildReport = BuildPipeline.BuildPlayer(getScenes(), buildArtifact,
+            BuildTarget.StandaloneWindows64, developmentBuild);
+        }catch(Exception error){
+            Debug.LogError(error);
+        }
+
+        stopLoggingErrors();
+        
+        if(buildReport!= null && buildReport.summary.result == BuildResult.Succeeded){
+            System.IO.File.WriteAllText(@"./builds/lastbuild.txt", $"./builds/{buildFolder}");
+        }else{
+            EditorApplication.Exit(1);
+        }
+    }
+
+    [MenuItem("Build/Android [Development]")]
+    static void PerformBuildAndroid()
+    {
+        string buildName = PlayerSettings.productName.Replace(" ", "-");
+        string buildFolder = "android";
+        string extension = "apk";
+        string buildArtifact = $"./builds/{buildFolder}/{buildName}.{extension}";
+        BuildReport buildReport = null;
+        startLoggingErrors();
+
+        try{
+            buildReport = BuildPipeline.BuildPlayer(getScenes(), buildArtifact,
+                BuildTarget.Android, developmentBuild);
+        }catch(Exception error){
+            Debug.LogError(error);
+        }
+
+        stopLoggingErrors();
+        
+        if(buildReport!= null && buildReport.summary.result == BuildResult.Succeeded){
+            System.IO.File.WriteAllText(@"./builds/lastbuild.txt", buildArtifact);
+        }else{
+            EditorApplication.Exit(1);
+        } 
+    }
+
+    [MenuItem("Build/iOS [Development]")]
+    static void PerformBuildIOS()
+    {
+        string buildName = "XcodeProject";
+        string buildFolder = "ios";
+        string buildArtifact = $"./builds/{buildFolder}/{buildName}";
+        BuildReport buildReport = null;
+        startLoggingErrors();
+
+        try{
+            buildReport = BuildPipeline.BuildPlayer(getScenes(), buildArtifact,
+                BuildTarget.iOS, developmentBuild);
+        }catch(Exception error){
+            Debug.LogError(error);
+        }
+
+        stopLoggingErrors();
+        
+        if(buildReport!= null && buildReport.summary.result == BuildResult.Succeeded){
+            System.IO.File.WriteAllText(@"./builds/lastbuild.txt", buildArtifact);
+        }else{
+            EditorApplication.Exit(1);
+        } 
+    }
+
+    private static string errorOutput;
+    private static void startLoggingErrors(){
+        errorOutput = "";
+        Application.logMessageReceived += logMessage;
+    }
+
+    private static void logMessage(string logString, string stackTrace, LogType type){
+        if(type == LogType.Error){
+            errorOutput += logString + "\n" + stackTrace + "\n";
+        }
+    }
+
+    private static void stopLoggingErrors(){
+        Application.logMessageReceived -= logMessage;
+        if(errorOutput != ""){
+            System.IO.File.WriteAllText(@"./builds/lastbuild/errorOutput.txt", errorOutput);
+        }
+    }
+}
---
+++ Assets/Scripts/Editor/UploadToS3.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+urlencode() {
+  python -c 'import html,sys; print html.escape(sys.argv[1], sys.argv[2])' \
+    "$1" "$urlencode_safe"
+}
+
+ExpiryTime=$(($(date +%s) + 7776000))
+ProjectName="GGJ2023"
+
+ScriptDirectory=$(dirname "$0")
+
+Platform=$1
+ArtifactPath=$2
+Extension=$3
+
+FileName=$ProjectName-$Platform-$(date +'%Y-%m')$Extension
+
+mv "$ArtifactPath" "$FileName"
+
+ArtifactURL="https://builds.connectedplay.io/$ProjectName/$FileName"
+InstallURL="https://builds.connectedplay.io/$ProjectName/install_$Platform.html"
+
+PrivateKey=~/CloudFront/private_connectedplay_builds_key.pem
+
+echo $FileName
+
+Policy="{
+    \"Statement\": [
+        {
+            \"Resource\": \"https://builds.connectedplay.io/$ProjectName/*\",
+            \"Condition\": {
+                \"DateLessThan\": {
+                    \"AWS:EpochTime\": $ExpiryTime
+                }
+            }
+        }
+    ]
+}"
+
+Base64Policy=$(echo "$Policy" | tr -d "\n" | tr -d " \t\n\r" | openssl base64 | tr -- '+=/' '-_~' | tr -d "\n" | tr -d " \t\n\r")
+SignedPolicy=$(echo "$Policy" | tr -d "\n" | tr -d " \t\n\r" | openssl sha1 -sign $PrivateKey | openssl base64 | tr -- '+=/' '-_~'| tr -d "\n" | tr -d " \t\n\r") 
+
+SignedArtifactURL="$ArtifactURL?Policy=$Base64Policy&Signature=$SignedPolicy&Key-Pair-Id=$CloudFrontKeyPairID"
+SignedInstallURL="$InstallURL?Policy=$Base64Policy&Signature=$SignedPolicy&Key-Pair-Id=$CloudFrontKeyPairID"
+
+DownloadURL=$(printf '%s\n' "$SignedArtifactURL" | gsed -e 's@[\/&]@\\&@g')
+
+FileSize=$(ls -lah $FileName | awk -F " " {'print $5'})
+
+echo "Replacing {build_number}"
+gsed -i 's@{build_number}@'$RevisionNumber'@g' "$ScriptDirectory/install.html"
+echo "Replacing {project_name}"
+gsed -i 's@{project_name}@'$ProjectName'@g' "$ScriptDirectory/install.html"
+echo "Replacing {platform_name}"
+gsed -i 's@{platform_name}@'$Platform'@g' "$ScriptDirectory/install.html"
+echo "Replacing {build_time}"
+gsed -i "s@{build_time}@$(date)@g" "$ScriptDirectory/install.html"
+echo "Replacing {file_size}"
+gsed -i 's@{file_size}@'$FileSize'@g' "$ScriptDirectory/install.html"
+echo "Replacing {download_url}"
+gsed -i 's@{download_url}@'$DownloadURL'@g' "$ScriptDirectory/install.html"
+
+aws s3 cp "$FileName" s3://builds.connectedplay.io/$ProjectName/$FileName --profile $AWSProfile
+aws s3 cp "$ScriptDirectory/install.html" s3://builds.connectedplay.io/$ProjectName/install_$Platform.html --profile $AWSProfile
+
+aws cloudfront create-invalidation \
+    --distribution-id $CloudFrontDistID \
+    --paths "/$ProjectName/*" --profile $AWSProfile
+
+GitVersionHash=$(git rev-parse --short HEAD)
+DateNow=$(date '+%Y-%m-%d %H:%M:%S')
+DiscordPostData="{\
+    \"content\": \"New [$Platform build #$GitVersionHash ($FileSize)]($SignedInstallURL) uploaded at $DateNow\"
+}"
+curl -i -H "Accept: application/json" -H "Content-Type:application/json" \
+-X POST --data "$DiscordPostData" "$DiscordWebhook"
